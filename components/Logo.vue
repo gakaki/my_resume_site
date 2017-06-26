2ac96c27f635e50c40de6db7ddb3cb644ab55a18
gakaki <gakaki@gmail.com>
2017-06-26 12:56:01 +0800

    deploy dist subtree commit

--- components/Logo.vue
+++ components/Logo.vue
@@ -59,6 +59,7 @@
       <div class="avatar">
         <img :src="portrait" alt="贺贤明的头像" />
         <h2>{{name}}</h2>
+        <p>{{positionStatus}}</p>
         <p>{{year}}</p>
         <p>{{job_title}}</p>
         <p>{{address}}</p>
@@ -166,13 +167,15 @@
 
     data: () => {
       return {
+        positionStatus: '离职正在找工作',
+        hope: '期望薪资 25k 税后 正常交金 正常情况下周六日不加班 如果是猎头请周六日和晚上安排面试吧 白天事情多..没法请假 ,要是每个都请假那现在也没法上班了...或者能否对方公司技术能先电话面试下。。在去实地面试，以节省双方时间',
         name: '贺贤明',
         email: 'gakaki@qq.com',
         qq: '82506111',
         portrait: 'https://avatars1.githubusercontent.com/u/5296?v=3&s=460',
         blog: 'http://blog.gakaki.com',
         github: `https://github.com/gakaki?tab=repositories`,
-        job_title: '救火工程师',
+        job_title: '伪全栈工程师',
         year: `84年 天蝎座`,
         college_degree: `大专`,
         college_school: `上海托普信息技术学院`,
@@ -213,18 +216,21 @@
         app_links: [
           {
             name: `尖叫设计`,
+            type: '家居电商',
             img: '',
             link: `https://itunes.apple.com/cn/app/jian-jiao-she-ji/id1110300308?mt=8`,
             desc: '一款设计清新,有社区拍图的 家居电商 App'
           },
           {
             name: `牛家帮-家政保洁,一键生活牛家帮！`,
+            type: 'O2O到家服务',
             img: '',
             link: 'https://itunes.apple.com/cn/app/niu-jia-bang-yi-jian-sheng/id887124712?mt=',
             desc: 'O2O 到家服务保洁'
           },
           {
             name: 'U糖健康—慢病管理远程托管平台',
+            type: '智能硬件',
             img: '',
             link: 'https://itunes.apple.com/cn/app/u%E7%B3%96%E5%81%A5%E5%BA%B7-%E6%85%A2%E7%97%85%E7%AE%A1%E7%90%86%E8%BF%9C%E7%A8%8B%E6%89%98%E7%AE%A1%E5%B9%B3%E5%8F%B0/id848886877?mt=8',
             desc: '智能糖尿病管理 + ARM智能医疗硬件'
@@ -232,12 +238,14 @@
           },
           {
             name: 'U糖服务端-协助医护服务糖友的好帮手',
+            type: '智能硬件',
             img: '',
             link: 'https://itunes.apple.com/cn/app/u-tang-yi-shi-xie-zhu-yi-shi/id919512425?mt=8',
             desc: '医生用版本'
           },
           {
             name: '申万宏源天游旗舰版',
+            type: '金融股票',
             img: '',
             link: 'https://itunes.apple.com/cn/app/shen-wan-hong-yuan-tian-you/id950920171?mt=8',
             desc: '股票交易所方出品App'
@@ -249,19 +257,19 @@
             link: 'http://www.wowdsgn.com',
             time_start: '16.01.18',
             time_end: '17.06.15',
-            job_title: '前技术负责人+ IOS高级软件研发工程师',
+            job_title: '前技术负责人+IOS高级研发',
             job_desc: [
               `组建四人团队(后端,前端H5,IOS)并成功在一个月内赶工完成,第一版'尖叫设计'App`,
               'PC网站端 http://www.wowdsgn.com (PHP Magento)UI模板的修改以及后续维护',
               'IOS客户端的代码 (Swift2.2编写),App相关第三方功能购买和整合.技术上用到的库有SnapKit,Moya,Rxswift,Objectmapper,Kinfisher,SDWebImage,UMeng,Talkingdata,MJRefresh,DZNEmptyDataSet,EZSwiftExtensions,HidingNavigationBar,YYImage,Pingpp,AVOSCloud,RongCloudIMKit.',
               'IOS App 服务器端 API编写.阿里云服务器的运维部署.包括Nginx php Nodejs ,SSL Lets Encrypt.技术上采用支持async的thinkjs框架,并使用redis缓存数据,mongodb作为数据库',
-              '管理AppStore的上架,审核,发布,fir.im持续集成.产品,设计和COO的协调工作',
+              '管理AppStore的上架,审核,发布,企业账号的申请发布,fir.im持续集成.产品,设计和COO的协调工作',
               '来了CTO之后',
               'PC网和移动端的图片的七牛迁移工作,编写脚本,HTTPS的申请,最终图片启用Webp格式(不支持的h5浏览器优雅降级jpg),优化网络传输.',
-              'Docker搭建Nexus库管理系统.编写Gradle SSH 脚本一键部署SpringBoot项目到服务器端.',
+              'Docker搭建Nexus库管理系统.编写Gradle 插件 SSH 脚本一键部署SpringBoot项目到服务器端.',
               '原网站PHP过渡到JAVA的数据迁移',
               '参与新版IOS App(下属2人),经历Swift2.2到Swift4的语言变迁,并主导了Swift3的大重构,参与新版IOS App(下属2人),现有项目Swift4 RxSwift响应式 组件二进制化进行中.',
-              '曾遭遇服务器恶意短信攻击, 并成功使用Redis和Openresty的Lua Waf模块来防御',
+              '曾遭遇服务器恶意短信攻击, 并成功使用Redis和Openresty的Lua Waf模块来防御.使用了Spark2.0 和 Python 来对nginx日志分析进行了重组和排序,找出对应的攻击ip并封锁.',
               'IOS App 客户端 TalkingData和友盟的数据埋点 和 UniversalLink的部署与微信的交互',
               'H5微信与IOS App交互功能开发,跳转下载,下载页面编写.活动页面与原生app交互.',
               'H5微信活动页面编写和功能实现,管理页面Vue.js 2.0 ElementUI 实现.',
@@ -322,7 +330,7 @@
             time_start: '2008.02',
             time_end: '2012.02',
             job_desc: [
-              '研发和维护 Shopex B2B B2C 产品,开发和Bug修复.新版本的ECOS的压力测试（libmemcached，tsung，多台Archlinux，mysql5.55beta）。使用Python编写多线程的B2B平台与淘宝API的数据同步，用到了MySQL Q4M 消息队列 . 维护网店的B2C的功能 , 当时使用的技术架构 php 5.2 + mysql + q4m + sphinx +tokey tryant + mootools',
+              '研发和维护 Shopex B2B B2C 产品,开发和Bug修复.新版本的ECOS的压力测试（libmemcached，tsung，多台Archlinux，mysql5.55beta）。使用Python 2.6 编写多线程的B2B平台与淘宝API的数据同步，用到了MySQL Q4M 消息队列 . 维护网店的B2C的功能 , 当时使用的技术架构 python 2.6 php 5.2 + mysql + q4m + sphinx +tokey tryant + mootools',
               '研发和维护 （名鞋库S.cn)的 IOS App 软件编写 11年中旬，并与后端系统进行数据交互。前端包括用户注册，登入，订单详情，首页幻灯，产品分类，购物车，收藏夹，帮助等功能。项目使用了 ASIHTTPREQUEST，AFNETWORKING,PullToRefresh，SDWebImage，JSONKIT ,Fmdb购物车，友盟等库，这里开始使用ARC方式进行 编程.2 编写移动端API (后端也是个人完成) 相关的php json api接口，使用linux php nginx mysql redis的开发环境,对现有管易B2C系统的数据编写 API JSON接口 ，涉及到短信注册，商品数据列表，用户登入注册，收藏夹，购物车等功能。',
               `'私货' APP sihuo.com 参与制作研发二手物品交易平台。职责为：用户注册登入，拍照，好友交互关注 ，个人中心模块，和聊天界面，商品评论，商品列表展示的功能。聊天使用环信API.`
             ]

b80ad86b27f62b212990e5397242397d143f36e6
gakaki <gakaki@gmail.com>
2018-07-25 14:14:07 +0800

    commit all

--- pdf_pupuetter.js
+++ pdf_pupuetter.js
@@ -1,7 +1,18 @@
 const puppeteer = require('puppeteer');
+/*
+https://segmentfault.com/a/1190000011382062
+
+#依赖库
+yum install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 -y
+
+#字体
+yum install ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc -y
+
+*/
+
 
 (async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
   const page = await browser.newPage();
   const url  = 'http://127.0.0.1:3000'
   await page.setViewport({width: 1024, height: 768 , deviceScaleFactor : 1})
